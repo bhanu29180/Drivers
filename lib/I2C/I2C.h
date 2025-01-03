5e8d13d2a3ed8c8372961c13833437306a0e9a8f
abhinayh56 <kumarabhinay53@gmail.com>
2025-01-03 09:45:47 +0530

    temp commit

--- lib/I2C/I2C.h
+++ lib/I2C/I2C.h
@@ -4,16 +4,17 @@
 #include  <Arduino.h>
 #include <Wire.h>
 
-class I2C{
-  public:
-    I2C();
-    void init(TwoWire* wire);
-    uint8_t read_register(uint8_t dev_addr, uint8_t reg_addr);
-    void read_registers(uint8_t dev_addr, uint8_t reg_addr, uint8_t num_bytes, uint8_t buff_[], uint8_t buff_i);
-    void write_register(uint8_t dev_addr, uint8_t reg_addr, uint8_t register_val);
-    void sendCommand(uint8_t dev_addr, uint8_t cmd);
-private:
-    TwoWire* _wire; 
+class I2C
+{
+	public:
+		I2C();
+		void init(TwoWire* wire);
+		uint8_t read_register(uint8_t dev_addr, uint8_t reg_addr);
+		void read_registers(uint8_t dev_addr, uint8_t reg_addr, uint8_t num_bytes, uint8_t buff_[], uint8_t buff_i);
+		void write_register(uint8_t dev_addr, uint8_t reg_addr, uint8_t register_val);
+		void sendCommand(uint8_t dev_addr, uint8_t cmd);
+	private:
+		TwoWire* _wire;
 };
 
 #endif
