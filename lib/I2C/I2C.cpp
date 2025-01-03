8b1e7b71e27d1541c31e90bbb941dd3afece6660
abhinayh56 <kumarabhinay53@gmail.com>
2025-01-03 08:31:23 +0530

    temp commit

--- lib/I2C/I2C.cpp
+++ lib/I2C/I2C.cpp
@@ -1,47 +1,46 @@
-#include "I2Cdev.h"
-#include "Wire.h"
+#include "I2C.h"
 
-I2Cdev::I2Cdev(){
-  //Wire.begin();
-  //TWBR = 12;
+I2C::I2C(){
 }
 
-void I2Cdev::initialize(){
-  Wire.begin();
-  //TWBR = 12;
-  Wire.setClock(400000);
+void I2C::init(TwoWire* wire){
+  _wire = /
+  333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333330.
+  wire;
+  _wire->begin();
+  _wire->setClock(400000);
 }
 
-uint8_t I2Cdev::read_register(uint8_t i2c_dev_addr, uint8_t reg_addr){
-  Wire.beginTransmission(i2c_dev_addr);
-  Wire.write(reg_addr);
-  Wire.endTransmission(false);
-  Wire.requestFrom(i2c_dev_addr,1,true);
-  while(Wire.available() < 1);
-  return Wire.read();
+uint8_t I2C::read_register(uint8_t dev_addr, uint8_t reg_addr){
+  _wire->beginTransmission(dev_addr);
+  _wire->write(reg_addr);
+  _wire->endTransmission(false);
+  _wire->requestFrom(dev_addr,1,true);
+  while(_wire->available() < 1);
+  return _wire->read();
 }
 
-void I2Cdev::read_registers(uint8_t i2c_dev_addr, uint8_t reg_addr, uint8_t n_bytes, uint8_t buff_[], uint8_t buff_i){
-  Wire.beginTransmission(i2c_dev_addr);
-  Wire.write(reg_addr);
-  Wire.endTransmission();
-  Wire.requestFrom(i2c_dev_addr,n_bytes);
-  while(Wire.available() < n_bytes);
+void I2C::read_registers(uint8_t dev_addr, uint8_t reg_addr, uint8_t n_bytes, uint8_t buff_[], uint8_t buff_i){
+  _wire->beginTransmission(dev_addr);
+  _wire->write(reg_addr);
+  _wire->endTransmission();
+  _wire->requestFrom(dev_addr,n_bytes);
+  while(_wire->available() < n_bytes);
   for(int i=buff_i;i<buff_i+n_bytes;i++){
-    buff_[i] = Wire.read();
+    buff_[i] = _wire->read();
   }
 }
 
-void I2Cdev::write_register(uint8_t i2c_dev_addr, uint8_t reg_addr, uint8_t register_val){
-  Wire.beginTransmission(i2c_dev_addr);
-  Wire.write(reg_addr);
-  Wire.write(register_val);
-  Wire.endTransmission();
+void I2C::write_register(uint8_t dev_addr, uint8_t reg_addr, uint8_t register_val){
+  _wire->beginTransmission(dev_addr);
+  _wire->write(reg_addr);
+  _wire->write(register_val);
+  _wire->endTransmission();
 }
 
-void I2Cdev::sendCommand(uint8_t i2c_dev_addr, uint8_t cmd)
+void I2C::sendCommand(uint8_t dev_addr, uint8_t cmd)
 {
-  Wire.beginTransmission(i2c_dev_addr);
-  Wire.write(cmd);
-  Wire.endTransmission();
+  _wire->beginTransmission(dev_addr);
+  _wire->write(cmd);
+  _wire->endTransmission();
 }
--- lib/I2C/I2C.h
+++ lib/I2C/I2C.h
@@ -1,16 +1,19 @@
-#ifndef I2CDEV_H
-#define I2CDEV_H
+#ifndef I2C_H
+#define I2C_H
 
 #include  <Arduino.h>
+#include <Wire.h>
 
-class I2Cdev{
+class I2C{
   public:
-    I2Cdev();
-    void initialize();
-    uint8_t read_register(uint8_t i2c_dev_addr, uint8_t reg_addr);
-    void read_registers(uint8_t i2c_dev_addr, uint8_t reg_addr, uint8_t num_bytes, uint8_t buff_[], uint8_t buff_i);
-    void write_register(uint8_t i2c_dev_addr, uint8_t reg_addr, uint8_t register_val);
-    void sendCommand(uint8_t i2c_dev_addr, uint8_t cmd);
+    I2C();
+    void init(TwoWire* wire);
+    uint8_t read_register(uint8_t dev_addr, uint8_t reg_addr);
+    void read_registers(uint8_t dev_addr, uint8_t reg_addr, uint8_t num_bytes, uint8_t buff_[], uint8_t buff_i);
+    void write_register(uint8_t dev_addr, uint8_t reg_addr, uint8_t register_val);
+    void sendCommand(uint8_t dev_addr, uint8_t cmd);
+private:
+    TwoWire* _wire; 
 };
 
 #endif
