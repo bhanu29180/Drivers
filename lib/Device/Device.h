f12f539caba29debb2e997e0e6aaee043c8c9b54
abhinayh56 <kumarabhinay53@gmail.com>
2025-01-07 09:25:47 +0530

    temp commit

--- lib/Device/Device.h
+++ lib/Device/Device.h
@@ -1,17 +1,19 @@
 #ifndef DEVICE_H
 #define DEVICE_H
 
-template <typename T>
+template <typename T_comm_bus, typename T_param>
 class Device
 {
     public:
-        void init();
-        void configure();
-		void read_data(uint8_t address, uint8_t* buffer);
-		void write_data(uint8_t address, uint8_t reg, uint8_t* values, uint8_t length);
+        Device();
+        virtual void configure();
+        virtual void init();
+		virtual void receive();
+		virtual void transmit();
 
     private:
-        T* comm_bus;
+        T_comm_bus* comm_bus;
+        T_param* param;
 };
 
 #endif
--- lib/I2C_bus/main_I2C_bus.cpp
+++ lib/I2C_bus/main_I2C_bus.cpp
@@ -7,9 +7,9 @@ I2C_bus<TwoWire> i2c2(&Wire2);
 
 void setup()
 {
-    i2c.init(400000);
-    i2c1.init(400000);
-    i2c2.init(400000);
+    i2c.init();
+    i2c1.init();
+    i2c2.init();
 }
 
 void loop()
