8c903070ce9f40e5bafc996dbd04166b357d7341
abhinayh56 <kumarabhinay53@gmail.com>
2025-01-15 02:19:21 +0530

    added HMC5883.cpp

---
+++ lib/HMC5883/HMC5883.h
@@ -0,0 +1,77 @@
+#ifndef HMC5883_H
+#define HMC5883_H
+
+#include <stdint.h>
+#include "utility.h"
+#include "HMC5883_reg.h"
+
+template <typename T_I2C_bus>
+class HMC5883
+{
+    public:
+        HMC5883(T_I2C_bus* i2c_bus_, uint8_t address_);
+        void init();
+        void config();
+
+        void get_mag(int16_t* mg_x, int16_t* mg_y, int16_t* mg_z);
+        void get_data(int16_t* mg_x, int16_t* mg_y, int16_t* mg_z);
+
+    private:
+        T_I2C_bus* i2c_bus;
+        uint8_t address;
+        uint8_t buff[6];
+
+        void set_bit_val(uint8_t reg, uint8_t bit_mask, uint8_t bit_lcn_0, uint8_t bit_val);
+        uint8_t get_bit_val(uint8_t reg, uint8_t bit_mask, uint8_t bit_lcn_0);
+};
+
+template <typename T_I2C_bus>
+HMC5883<T_I2C_bus>::HMC5883(T_I2C_bus* i2c_bus_, uint8_t address_)
+{
+}
+
+template <typename T_I2C_bus>
+void HMC5883<T_I2C_bus>::init()
+{
+    set_bit_val((uint8_t)HMC5883_INFO::REGISTER::CONFIG_A, (uint8_t)BIT::MASK8::BITM1_7,(uint8_t)SEVEN,(uint8_t)0);
+    set_bit_val((uint8_t)HMC5883_INFO::REGISTER::CONFIG_B, (uint8_t)BIT::MASK8::BITM5_0,(uint8_t)ZERO,(uint8_t)0);
+}
+
+template <typename T_I2C_bus>
+void HMC5883<T_I2C_bus>::config()
+{
+}
+
+template <typename T_I2C_bus>
+void HMC5883<T_I2C_bus>::get_mag(int16_t* mg_x, int16_t* mg_y, int16_t* mg_z)
+{
+    i2c_bus->read_registers(address,HMC5883_INFO::REGISTER::DATA_OUT_X_MSB, buff, 6);
+    *mg_x = ((int16_t)buff[0] << 8) | buff[1];
+    *mg_z = ((int16_t)buff[2] << 8) | buff[3];
+    *mg_y = ((int16_t)buff[4] << 8) | buff[5];
+}
+
+template <typename T_I2C_bus>
+void HMC5883<T_I2C_bus>::get_data(int16_t* mg_x, int16_t* mg_y, int16_t* mg_z)
+{
+    i2c_bus->read_registers(address,HMC5883_INFO::REGISTER::DATA_OUT_X_MSB, buff, 6);
+    *mg_x = ((int16_t)buff[0] << 8) | buff[1];
+    *mg_z = ((int16_t)buff[2] << 8) | buff[3];
+    *mg_y = ((int16_t)buff[4] << 8) | buff[5];
+}
+
+template <typename T_I2C_bus>
+void HMC5883<T_I2C_bus>::set_bit_val(uint8_t reg, uint8_t bit_mask, uint8_t bit_lcn_0, uint8_t bit_val)
+{
+    uint8_t register_val = i2c_bus->read_register(address, reg);
+    register_val = (register_val & (~bit_mask)) | (bit_val<<bit_lcn_0);
+    i2c_bus->write_register(address, reg, register_val);
+}
+
+template <typename T_I2C_bus>
+uint8_t HMC5883<T_I2C_bus>::get_bit_val(uint8_t reg, uint8_t bit_mask, uint8_t bit_lcn_0)
+{
+    return ((i2c_bus->read_register(address, reg)) & bit_mask) >> bit_lcn_0;
+}
+
+#endif
