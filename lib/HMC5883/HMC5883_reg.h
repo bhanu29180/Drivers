6bf6065d383f0b716319887ac64c5a0b04a64cac
abhinayh56 <kumarabhinay53@gmail.com>
2025-01-14 13:19:43 +0530

    modified HMC5883

--- lib/HMC5883/HMC5883_reg.h
+++ lib/HMC5883/HMC5883_reg.h
@@ -1,7 +1,9 @@
 #ifndef HMC5883_REG
 #define HMC5883_REG
 
-namespace HMC5883
+#include <stdint.h>
+
+namespace HMC5883_INFO
 {
     enum class ADDR
     {
@@ -9,7 +11,7 @@ namespace HMC5883
         I2C_DEFAULT = I2C
     };
 
-    enum class REGISTER
+    enum class REGISTER : uint8_t
     {
         CONFIG_A         = 0x00,
         CONFIG_B         = 0x01,
@@ -25,6 +27,74 @@ namespace HMC5883
         IDENTIFICATION_B = 0x0B,
         IDENTIFICATION_C = 0x0C
     };
+    namespace BIT_FLAGS
+    {
+        enum class CONFIG_A : uint8_t
+        {
+            MA_0 = 0,
+            MA_1 = 1,
+            MA_2 = 2,
+            MA_3 = 3,
+            DO_0 = 0,
+            DO_1 = 1,
+            DO_2 = 2,
+            DO_3 = 3,
+            DO_4 = 4,
+            DO_5 = 5,
+            DO_6 = 6,
+            DO_7 = 7,
+            MS_0 = 0,
+            MS_1 = 1,
+            MS_2 = 2,
+            MS_3 = 3
+        };
+
+        enum class CONFIG_B : uint8_t
+        {
+            GN_0 = 0,
+            GN_1 = 1,
+            GN_2 = 2,
+            GN_3 = 3,
+            GN_4 = 4,
+            GN_5 = 5,
+            GN_6 = 6,
+            GN_7 = 7
+        };
+
+        enum class MODE : uint8_t
+        {
+            HS_0 = 0,
+            HS_1 = 1,
+            MD_0 = 0,
+            MD_1 = 1,
+            MD_2 = 2,
+            MD_3 = 3
+        };
+
+        enum class STATUS : uint8_t
+        {
+            LOCK_0 = 0,
+            LOCK_1 = 1,
+            RDY_0  = 0,
+            RDY_1  = 1
+        };
+
+        enum class IDENTIFICATION_A : uint8_t
+        {
+            IRA = (1<<6) | (1<<3)
+        };
+
+        enum class IDENTIFICATION_B : uint8_t
+        {
+            IRB = (1<<5) | (1<<4) | (1<<2)
+        };
+
+        enum class IDENTIFICATION_C : uint8_t
+        {
+            IRC = (1<<5) | (1<<4) | (1<<1) | (1<<0)
+        };
+
+    }
 }
 
 #endif
