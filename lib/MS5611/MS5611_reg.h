b18b9fc8743c47518e19eff417f314dd76b2eb98
abhinayh56 <kumarabhinay53@gmail.com>
2025-01-20 19:59:54 +0530

    temp commit

--- lib/MS5611/MS5611_reg.h
+++ lib/MS5611/MS5611_reg.h
@@ -15,21 +15,17 @@ namespace MS5611_INFO
     enum class COMMAND : uint8_t
     {
         RESET               = 0x1E,
-
         CONVERT_D1_OSR_256  = 0x40,
         CONVERT_D1_OSR_512  = 0x42,
         CONVERT_D1_OSR_1024 = 0x44,
         CONVERT_D1_OSR_2048 = 0x46,
         CONVERT_D1_OSR_4096 = 0x48,
-
         CONVERT_D2_OSR_256  = 0x50,
         CONVERT_D2_OSR_512  = 0x52,
         CONVERT_D2_OSR_1024 = 0x54,
         CONVERT_D2_OSR_2048 = 0x56,
         CONVERT_D2_OSR_4096 = 0x58,
-
         ADC_READ            = 0x00,
-
         PROM_READ_0         = 0xA0,
         PROM_READ_1         = 0xA2,
         PROM_READ_2         = 0xA4,
