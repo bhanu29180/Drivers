dceeb4ef89b0345bb31788547516b60b9c5badfd
abhinayh56 <kumarabhinay53@gmail.com>
2025-01-21 10:23:43 +0530

    modified i2c_scanner

--- lib/I2C_bus/I2C_bus.ino
+++ lib/I2C_bus/I2C_bus.ino
@@ -1,48 +1,58 @@
 #include "Arduino.h"
 #include <Wire.h>
+#include "I2C_bus.h"
+
+I2C_bus<TwoWire> i2c_bus(&Wire);
 
 void setup()
 {
-    Serial.begin(115200);
-    Wire.begin();
-    Wire.setClock(400000);
+	Serial.begin(115200);
+	i2c_bus.init();
+	i2c_bus.config(400000);
 }
 
 void loop()
 {
-  byte error, address;
-  int nDevices;
+	Serial.println("Scanning...");
 
-  Serial.println("Scanning...");
+	int nDevices = 0;
+	
+	for(uint8_t address = 1; address < 127; address++) 
+	{
+		Wire.beginTransmission(address);
+		uint8_t error = Wire.endTransmission();
 
-  nDevices = 0;
-  for(address = 1; address < 127; address++ ) 
-  {
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
+		if (error == 0)
+		{
+			Serial.print("I2C device found at address 0x");
+			if (address<16) 
+			{
+				Serial.print("0");
+			}
+			Serial.print(address,HEX);
+			Serial.println("  !");
 
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address<16) 
-        Serial.print("0");
-      Serial.print(address,HEX);
-      Serial.println("  !");
+			nDevices++;
+		}
+		else if (error==4) 
+		{
+			Serial.print("Unknown error at address 0x");
+			if (address<16) 
+			{
+				Serial.print("0");
+			}
+			Serial.println(address,HEX);
+		}    
+	}
 
-      nDevices++;
-    }
-    else if (error==4) 
-    {
-      Serial.print("Unknown error at address 0x");
-      if (address<16) 
-        Serial.print("0");
-      Serial.println(address,HEX);
-    }    
-  }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println("done\n");
+	if (nDevices == 0)
+	{
+		Serial.println("No I2C devices found\n");
+	}
+	else
+	{
+		Serial.println("done\n");
+	}
 
-  delay(5000);           // wait 5 seconds for next scan
+	delay(5000);
 }
