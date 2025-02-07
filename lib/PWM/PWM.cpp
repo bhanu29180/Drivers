b5b031cd89237a1d5087dc6309d6b1263180cdde
abhinayh56 <kumarabhinay53@gmail.com>
2025-02-07 15:42:29 +0530

    modified pwm class

--- lib/PWM/PWM.cpp
+++ lib/PWM/PWM.cpp
@@ -4,7 +4,7 @@ PWM::PWM()
 {
 }
 
-void PWM::init(float pwm1=1000, float pwm2=1000, float pwm3=1000, float pwm4=1000)
+void PWM::init()
 {
     pinMode(motor_1,OUTPUT);
     pinMode(motor_2,OUTPUT);
@@ -12,8 +12,13 @@ void PWM::init(float pwm1=1000, float pwm2=1000, float pwm3=1000, float pwm4=100
     pinMode(motor_4,OUTPUT);
 }
 
-void PWM::config()
+void PWM::config(float pwm_min_us_, float pwm_max_us_, uint32_t resolution_, float freq_)
 {
+    pwm_min_us = pwm_min_us_;
+    pwm_max_us = pwm_max_us_;
+    resolution = resolution_;
+    freq = freq_;
+
     analogWriteFrequency(motor_1, freq);
     analogWriteFrequency(motor_2, freq);
     analogWriteFrequency(motor_3, freq);
@@ -21,11 +26,16 @@ void PWM::config()
 
     analogWriteResolution(resolution);
 
-    set_pwm(1000.0,1000.0,1000.0,1000.0);
+    set_pwm(pwm_min_us, pwm_min_us, pwm_min_us, pwm_min_us);
 }
 
 void PWM::set_pwm(float pwm1=1000, float pwm2=1000, float pwm3=1000, float pwm4=1000)
 {
+    pwm1 = saturate(pwm1, pwm_min_us, pwm_max_us);
+    pwm2 = saturate(pwm2, pwm_min_us, pwm_max_us);
+    pwm3 = saturate(pwm3, pwm_min_us, pwm_max_us);
+    pwm4 = saturate(pwm4, pwm_min_us, pwm_max_us);
+
     analogWriteResolution(resolution);
     analogWrite(motor_1,pwm_2_bit_val(pwm1));
     analogWrite(motor_2,pwm_2_bit_val(pwm2));
@@ -36,6 +46,7 @@ void PWM::set_pwm(float pwm1=1000, float pwm2=1000, float pwm3=1000, float pwm4=
 
 void PWM::set_pwm_1(float pwm1)
 {
+    pwm1 = saturate(pwm1, pwm_min_us, pwm_max_us);
     analogWriteResolution(resolution);
     analogWrite(motor_1,pwm_2_bit_val(pwm1));
     analogWriteResolution(resolution);
@@ -43,6 +54,7 @@ void PWM::set_pwm_1(float pwm1)
 
 void PWM::set_pwm_2(float pwm2)
 {
+    pwm2 = saturate(pwm2, pwm_min_us, pwm_max_us);
     analogWriteResolution(resolution);
     analogWrite(motor_2,pwm_2_bit_val(pwm2));
     analogWriteResolution(resolution);
@@ -50,6 +62,7 @@ void PWM::set_pwm_2(float pwm2)
 
 void PWM::set_pwm_3(float pwm3)
 {
+    pwm3 = saturate(pwm3, pwm_min_us, pwm_max_us);
     analogWriteResolution(resolution);
     analogWrite(motor_3,pwm_2_bit_val(pwm3));
     analogWriteResolution(resolution);
@@ -57,6 +70,7 @@ void PWM::set_pwm_3(float pwm3)
 
 void PWM::set_pwm_4(float pwm4)
 {
+    pwm4 = saturate(pwm4, pwm_min_us, pwm_max_us);
     analogWriteResolution(resolution);
     analogWrite(motor_4,pwm_2_bit_val(pwm4));
     analogWriteResolution(resolution);
@@ -65,4 +79,20 @@ void PWM::set_pwm_4(float pwm4)
 int PWM::pwm_2_bit_val(float pwm)
 {
     return 13.1072*pwm; //(32768.0/2500.0)*pwm; // ((2^PWM_resolution) / (1,000,000/PWM_frequency)) * pwm
+}
+
+float PWM::saturate(float x, float x_min, float x_max)
+{
+    if(x < x_min)
+    {
+        return x_min;
+    }
+    else if (x > x_max)
+    {
+        return x_max;
+    }
+    else{
+        return x;
+    }
+    
 }
--- lib/PWM/PWM.h
+++ lib/PWM/PWM.h
@@ -8,8 +8,8 @@ class PWM
 {
     public:
         PWM();
-        void init(float pwm1=1000, float pwm2=1000, float pwm3=1000, float pwm4=1000);
-        void config();
+        void init();
+        void config(float pwm_min_=1000.0, float pwm_max_=2000.0, uint32_t resolution_=15, float freq_=400.0);
         void set_pwm(float pwm1=1000, float pwm2=1000, float pwm3=1000, float pwm4=1000);
         void set_pwm_1(float pwm1);
         void set_pwm_2(float pwm2);
@@ -23,8 +23,11 @@ class PWM
         uint8_t motor_4 = 6;
         float freq = 400; // in Hz (i.e. T = 2500 us)
         uint32_t resolution = 15; // 32768
+        float pwm_min_us = 1000.0;
+        float pwm_max_us = 2000.0;
 
         int pwm_2_bit_val(float pwm);
+        float saturate(float x, float x_min, float x_max);
 };
 
 #endif
