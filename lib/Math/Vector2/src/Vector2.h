e5120430b16f5c6bd2cc2b0283d2c95d47116193
abhinayh56 <kumarabhinay53@gmail.com>
2025-02-17 19:34:47 +0530

    temp commit

--- lib/Math/Vector2/src/Vector2.h
+++ lib/Math/Vector2/src/Vector2.h
@@ -25,7 +25,6 @@ class Vector2
         static inline constexpr Vector2<T> div(const Vector2<T>& v, T s);
         static inline constexpr Vector2<T> div(T s, const Vector2<T>& v);
 
-        // Static member functions
         static inline constexpr T dot(const Vector2<T>& v1, const Vector2<T>& v2);
         static inline constexpr T cross(const Vector2<T>& v1, const Vector2<T>& v2);
         static inline constexpr Vector2<T> normalize(const Vector2<T>& v);
