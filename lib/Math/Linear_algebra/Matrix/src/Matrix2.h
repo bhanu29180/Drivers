64e5d575ed192f92b76e1550c1ab83e74dd933ed
abhinayh56 <kumarabhinay53@gmail.com>
2025-02-19 22:35:31 +0530

    temp commit

--- lib/Math/Linear_algebra/Matrix/src/Matrix2.h
+++ lib/Math/Linear_algebra/Matrix/src/Matrix2.h
@@ -18,9 +18,11 @@ public:
     static inline constexpr Matrix2<T> zero();
     static inline constexpr Matrix2<T> ones();
     static inline constexpr Matrix2<T> identity();
+
     static inline constexpr Matrix2<T> add(const Matrix2<T>& M1, const Matrix2<T>& M2);
     static inline constexpr Matrix2<T> sub(const Matrix2<T>& M1, const Matrix2<T>& M2);
     static inline constexpr Matrix2<T> mul(const Matrix2<T>& M1, const Matrix2<T>& M2);
+
     static inline constexpr Matrix2<T> add(const Matrix2<T>& M1, T val);
     static inline constexpr Matrix2<T> add(T val, const Matrix2<T>& M1);
     static inline constexpr Matrix2<T> sub(const Matrix2<T>& M1, T val);
@@ -28,7 +30,7 @@ public:
     static inline constexpr Matrix2<T> mul(const Matrix2<T>& M1, T val);
     static inline constexpr Matrix2<T> mul(T val, const Matrix2<T>& M1);
     static inline constexpr Matrix2<T> div(const Matrix2<T>& M1, T val);
-    static inline constexpr Matrix2<T> transpose(const Matrix2<T>& M);
+    static inline constexpr Matrix2<T> trans(const Matrix2<T>& M);
     static inline constexpr T det(const Matrix2<T>& M);
     static inline constexpr T trace(const Matrix2<T>& M);
     static inline constexpr Matrix2<T> minor(const Matrix2<T>& M);
@@ -57,9 +59,6 @@ public:
     Matrix2<T>& operator/=(T scalar);
     Matrix2<T>& operator%=(T scalar);
 
-    T& operator()(int row, int col);
-    constexpr T operator()(int row, int col) const;
-    
     constexpr bool operator==(const Matrix2<T>& other) const;
     constexpr bool operator!=(const Matrix2<T>& other) const;
 
@@ -67,6 +66,9 @@ public:
     bool isSingular(const Matrix2<T>& M) const;
     bool isIdentity(const Matrix2<T>& M) const;
     bool isSymmetric(const Matrix2<T>& M) const;
+
+    T& operator()(int row, int col);
+    constexpr T operator()(int row, int col) const;
 };
 
 #include "Matrix2.tpp"
