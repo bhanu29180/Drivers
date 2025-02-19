19c44bf70a8ad1e576e38b29b5733d19f90f3998
abhinayh56 <kumarabhinay53@gmail.com>
2025-02-19 17:31:27 +0530

    temp commit

---
+++ lib/Math/Linear_algebra/Vector/test/test_vector2.cpp
@@ -0,0 +1,21 @@
+#include "iostream"
+#include "../src/Vector2.h"
+
+int main()
+{
+    std::cout << "---" << std::endl;
+    std::cout << "TEST: Vector2" << std::endl;
+
+    Vector2<int> v1(4,9);
+
+
+
+
+
+
+
+
+    std::cout << "---" << std::endl;
+
+    return 0;
+}
